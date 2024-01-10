55c6f196d3dbfd5b5dbc28f2de29ba90a26aac69
abhishekgovindrao <atulgovindrao@gmail.com>
2024-01-10 06:47:55 +0000

    lecture 4 done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountID =12345
+let acccountEmail = "abhishek@gmail"
+var accountPassword = "12345"
+acocuntCity ="Jaipur"
+console.table([accountID, acccountEmail, accountPassword ,acocuntCity])
+
+acccountEmail ="AGRa@gmail.com"
+accountPassword ="2322"
+acocuntCity = "LKO"
+
+console.table([accountID, acccountEmail, accountPassword ,acocuntCity])
+
+/*
+ prefer not to use var
+ becox of issue in block sccope and functional scope
+*/
